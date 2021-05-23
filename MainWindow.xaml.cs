659f9b6c007cd0512bbee355fa04b6f82533fafe
vijayarasan <58214164+vijayarasan@users.noreply.github.com>
2021-05-23 23:17:39 +0530

    Sample added
    Sample added

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace SfDataGridDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class ColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //Get the DataRowView
+            var dataRow = value as DataRowView;
+
+            //Get the colors name value
+            var record = dataRow.Row["Colors"].ToString();
+
+            //You can use a BrushConverter
+            SolidColorBrush redBrush = (SolidColorBrush)new BrushConverter().ConvertFromString(record);
+
+            //return the color based on DataBase cell value
+            return redBrush;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System.Data;
+
+namespace SfDataGridDemo
+{
+    public class ViewModel
+    {
+        private DataTable employeeCollection;
+
+        public ViewModel()
+        {
+            this.EmployeeDetails = this.GetTable();
+        }
+
+        public DataTable EmployeeDetails
+        {
+            get { return employeeCollection; }
+            set { employeeCollection = value; }
+        }
+
+        public DataTable GetTable()
+        {
+            EmployeeDetails = new DataTable();
+            EmployeeDetails.Columns.Add("EmployeeID", typeof(int));
+            EmployeeDetails.Columns[0].ColumnName = "Employee ID";
+            EmployeeDetails.Columns.Add("EmployeeName", typeof(string));
+            EmployeeDetails.Columns[1].ColumnName = "Employee Name";
+            EmployeeDetails.Columns.Add("CustomerID", typeof(string));
+            EmployeeDetails.Columns[2].ColumnName = "Customer ID";
+            EmployeeDetails.Columns.Add("Country", typeof(string));            
+            EmployeeDetails.Columns.Add("Colors", typeof(string));           
+
+            EmployeeDetails.Rows.Add(1001, "Belgim", "Britain", "US", "LightBlue");
+            EmployeeDetails.Rows.Add(1002, "Oliver", "Oregon", "UK", "Red");
+            EmployeeDetails.Rows.Add(1003, "Bernald", "Alfki", "US", "Green");
+            EmployeeDetails.Rows.Add(1004, "James", "Anatr", "Chicago", "Yellow");
+            EmployeeDetails.Rows.Add(1005, "Beverton", "Bergs", "Spain", "Pink");
+            EmployeeDetails.Rows.Add(1006, "Fransis", "Alfki", "US", "LightBlue");
+            EmployeeDetails.Rows.Add(1007, "George", "Oregon", "SwitzerLand", "Red");
+            EmployeeDetails.Rows.Add(1008, "Dintin", "Britain", "Britain", "Green");
+            EmployeeDetails.Rows.Add(1009, "Diano", "Alfki", "Britain", "Yellow");
+            EmployeeDetails.Rows.Add(1010, "Joysie", "Oregon", "China", "Pink");
+            return EmployeeDetails;
+        }
+    }
+}
